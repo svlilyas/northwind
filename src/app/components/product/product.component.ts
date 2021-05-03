034e5be2e9a281bea2bb512dcda414c3db942248
ProAndroCoder <ilyas.savli47@gmail.com>
2021-05-03 12:16:57 +0300

    17. Ders Sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -9,8 +10,18 @@ import { Product } from 'src/app/models/product';
 export class ProductComponent implements OnInit {
 
   products: Product[] = [];
+  dataLoaded=false;
 
-  constructor() {}
+  constructor(private productservice: ProductService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productservice.getProducts().subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true
+    } )
+  }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiURL="https://localhost:44329/api/products/getall"
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<ProductResponseModel>{
+    return this.httpClient.get<ProductResponseModel>(this.apiURL)
+  }
+}
