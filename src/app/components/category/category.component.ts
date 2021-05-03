19827d56ade7f912be89496cc392dd946e61916e
ProAndroCoder <ilyas.savli47@gmail.com>
2021-05-03 10:57:48 +0300

    Modeller Eklendi

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
+  category1 = { categoryId: 1, categoryName: 'Bardak',  unitPrice: 5 }
+  category2 = { categoryId: 2, categoryName: 'Laptop',  unitPrice: 5 }
+  category3 = { categoryId: 3, categoryName: 'Mouse',  unitPrice: 5 }
+  category4 = { categoryId: 4, categoryName: 'Keyboard', unitPrice: 5 }
+  category5 = { categoryId: 5, categoryName: 'Camera', unitPrice: 5 }
+
+  categories = [this.category1, this.category2, this.category3, this.category4, this.category5];
+
   constructor() { }
 
   ngOnInit(): void {
