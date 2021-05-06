a942ec88bfd1891f2b69e3ff0565cef4f57eb401
ProAndroCoder <ilyas.savli47@gmail.com>
2021-05-06 13:33:07 +0300

    20. Ders 1. Kısım

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 
 
@@ -23,5 +24,9 @@ export class ProductService {
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
     let newPath=this.apiURL+"products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath)
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiURL+"products/add",product)
   }
 }
